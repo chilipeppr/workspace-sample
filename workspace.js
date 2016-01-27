3634978449aae76ba6e2b492991b4c808d07f417
John Lauer <jlauer12@gmail.com>
2016-01-27 06:20:08 +0000

    Made some changes to ChiliPeppr myWorkspace using Cloud9

--- workspace.js
+++ workspace.js
@@ -46,6 +46,16 @@ cpdefine("inline:com-chilipeppr-workspace-sample", ["chilipeppr_ready"], functio
         githuburl: "(auto fill by runme.js)", // The backing github repo
         testurl: "(auto fill by runme.js)", // The standalone working widget so can view it working by itself
         /**
+         * Contains reference to the Console widget object. Hang onto the reference
+         * so we can resize it when the window resizes because we want it to manually
+         * resize to fill the height of the browser so it looks clean.
+         */
+        widgetConsole: null,
+        /**
+         * Contains reference to the Serial Port JSON Server object.
+         */
+        widgetSpjs: null,
+        /**
          * The workspace's init method. It loads the all the widgets contained in the workspace
          * and inits them.
          */
@@ -53,10 +63,21 @@ cpdefine("inline:com-chilipeppr-workspace-sample", ["chilipeppr_ready"], functio
 
             // Most workspaces will instantiate the Serial Port JSON Server widget
             this.loadSpjsWidget();
+            // Most workspaces will instantiate the Serial Port Console widget
+            this.loadConsoleWidget(function() {
+                setTimeout(function() { $(window).trigger('resize'); }, 100);
+            });
             // Create our workspace upper right corner triangle menu
             this.loadWorkspaceMenu();
             // Add our billboard to the menu (has name, url, picture of workspace)
             this.addBillboardToWorkspaceMenu();
+            
+            // Setup an event to react to window resize. This helps since
+            // some of our widgets have a manual resize to cleanly fill
+            // the height of the browser window. You could turn this off and
+            // just set widget min-height in CSS instead
+            this.setupResize();
+            setTimeout(function() { $(window).trigger('resize'); }, 100);
 
         },
         /**
@@ -78,6 +99,18 @@ cpdefine("inline:com-chilipeppr-workspace-sample", ["chilipeppr_ready"], functio
             // get copy of billboard
             var billboardEl = this.getBillboard();
             $('#' + this.id + ' .com-chilipeppr-ws-billboard').append(billboardEl);
+        },
+        /**
+         * Listen to window resize event.
+         */
+        setupResize: function() {
+            $(window).on('resize', this.onResize.bind(this));
+        },
+        /**
+         * When browser window resizes, forcibly resize the Console window
+         */
+        onResize: function() {
+            if (this.widgetConsole) this.widgetConsole.resize();
         },
         /**
          * Load the Serial Port JSON Server widget via chilipeppr.load()
@@ -103,9 +136,42 @@ cpdefine("inline:com-chilipeppr-workspace-sample", ["chilipeppr_ready"], functio
                         //spjs.showBody();
                         //spjs.consoleToggle();
 
+                        that.widgetSpjs - spjs;
+                        
                         if (callback) callback(spjs);
 
                     });
+                }
+            );
+        },
+        /**
+         * Load the Console widget via chilipeppr.load()
+         */
+        loadConsoleWidget: function(callback) {
+            var that = this;
+            chilipeppr.load(
+                "#com-chilipeppr-widget-spconsole-instance",
+                "http://fiddle.jshell.net/chilipeppr/rczajbx0/show/light/",
+                function() {
+                    // Callback after widget loaded into #com-chilipeppr-widget-spconsole-instance
+                    cprequire(
+                        ["inline:com-chilipeppr-widget-spconsole"], // the id you gave your widget
+                        function(mywidget) {
+                            // Callback that is passed reference to your newly loaded widget
+                            console.log("My Console widget just got loaded.", mywidget);
+                            that.widgetConsole = mywidget;
+                            
+                            // init the serial port console
+                            // 1st param tells the console to use "single port mode" which
+                            // means it will only show data for the green selected serial port
+                            // rather than for multiple serial ports
+                            // 2nd param is a regexp filter where the console will filter out
+                            // annoying messages you don't generally want to see back from your
+                            // device, but that the user can toggle on/off with the funnel icon
+                            that.widgetConsole.init(true, /myfilter/);
+                            if (callback) callback(mywidget);
+                        }
+                    );
                 }
             );
         },
